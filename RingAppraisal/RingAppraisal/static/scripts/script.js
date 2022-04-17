9f4cbe26d2a10cd069c58d8c157c7c6f5031b1e5
aaron <aaronliftig@gmail.com>
2022-04-17 12:28:59 +1000

    Created hidding dropdowns

--- RingAppraisal/RingAppraisal/static/scripts/script.js
+++ RingAppraisal/RingAppraisal/static/scripts/script.js
@@ -1,9 +1,41 @@
+$(document).ready(function () {
+    d3.json("./static/scripts/constants.json").then((data) => {
+        data = data[0]
+        for (const val in data.primary_stone) {
+            const valDict = data.primary_stone[val];
+            if (Object.keys(valDict).length !== 0) {
+                for (const prop in valDict) {
+                    $("#primary-" + val + "-" + prop + "-dropdown select").hide();
+                    $("#primary-" + val + "-radio").change(function () {
+                        if ($(this).is(":checked")) {
+                            $("#primary-" + val + "-" + prop + "-dropdown select").show();
+                            console.log(1);
+                        } else {
+                            $("#primary-" + val + "-" + prop + "-dropdown select").hide();
+                            console.log(2);
+                        }
+                    })
+                }
+            }
+        }
 
-//$('#primary-' + val + '-' + prop + '-' + propVal + ' select').hide(); //initially dropdown is hidden
-//$('#primary-' + val).change(function () { //change click to hidden
-//                            if ($(this).is(':checked')) { //check if checkbox is checked
-//    $('#primary-' + val + '-' + prop + '-' + propVal + ' select').show(); //show if checked
-//                            } else {
-//    $('#primary-' + val + '-' + prop + '-' + propVal + ' select').hide(); //hide if unchecked
-//                            }
-//                        })
+        for (const val in data.secondary_stones) {
+            const valDict = data.secondary_stones[val];
+            if (Object.keys(valDict).length !== 0) {
+                for (const prop in valDict) {
+                    const idVal = val.replace(" ","-")
+                    $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
+                    $("#secondary-" + idVal + "-checkbox").change(function () {
+                        if ($(this).is(":checked")) {
+                            $("#secondary-" + idVal + "-" + prop + "-dropdown select").show();
+                            console.log(1);
+                        } else {
+                            $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
+                            console.log(2);
+                        }
+                    })
+                }
+            }
+        }
+    });
+})
