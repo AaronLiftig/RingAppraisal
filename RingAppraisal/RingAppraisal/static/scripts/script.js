4c9d44ff17967c63dba3152b8ae40cf96ceaaf87
aaron <aaronliftig@gmail.com>
2022-04-18 13:43:47 +1000

    Added and fixed dropdowns

--- RingAppraisal/RingAppraisal/static/scripts/script.js
+++ RingAppraisal/RingAppraisal/static/scripts/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     d3.json("./static/scripts/constants.json").then((data) => {
         data = data[0]
+
         for (const val in data.primary_stone) {
             const valDict = data.primary_stone[val];
             if (Object.keys(valDict).length !== 0) {
@@ -9,10 +10,8 @@ $(document).ready(function () {
                     $("#primary-" + val + "-radio").change(function () {
                         if ($(this).is(":checked")) {
                             $("#primary-" + val + "-" + prop + "-dropdown select").show();
-                            console.log(1);
                         } else {
                             $("#primary-" + val + "-" + prop + "-dropdown select").hide();
-                            console.log(2);
                         }
                     })
                 }
@@ -23,15 +22,14 @@ $(document).ready(function () {
             const valDict = data.secondary_stones[val];
             if (Object.keys(valDict).length !== 0) {
                 for (const prop in valDict) {
-                    const idVal = val.replace(" ","-")
+                    const idVal = val.replaceAll(" ", "-")
                     $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
                     $("#secondary-" + idVal + "-checkbox").change(function () {
+                        $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
                         if ($(this).is(":checked")) {
                             $("#secondary-" + idVal + "-" + prop + "-dropdown select").show();
-                            console.log(1);
                         } else {
                             $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
-                            console.log(2);
                         }
                     })
                 }
