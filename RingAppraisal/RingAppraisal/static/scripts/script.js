9c2f2dc18fe0e0b0e273e59ee51fb9121fea0662
aaron <aaronliftig@gmail.com>
2022-04-18 16:18:27 +1000

    Fixed radio buttons not revealing dropdown options, and other formating issues.

--- RingAppraisal/RingAppraisal/static/scripts/script.js
+++ RingAppraisal/RingAppraisal/static/scripts/script.js
@@ -2,34 +2,32 @@ $(document).ready(function () {
     d3.json("./static/scripts/constants.json").then((data) => {
         data = data[0]
 
+        $(".primary-property-radio").hide()
         for (const val in data.primary_stone) {
             const valDict = data.primary_stone[val];
-            if (Object.keys(valDict).length !== 0) {
-                for (const prop in valDict) {
-                    $("#primary-" + val + "-" + prop + "-dropdown select").hide();
-                    $("#primary-" + val + "-radio").change(function () {
-                        if ($(this).is(":checked")) {
-                            $("#primary-" + val + "-" + prop + "-dropdown select").show();
-                        } else {
-                            $("#primary-" + val + "-" + prop + "-dropdown select").hide();
+            $("#primary-" + val + "-radio").change(function () {
+                $(".primary-property-radio").hide()
+                if ($(this).is(":checked")) {
+                    if (Object.keys(valDict).length !== 0) {
+                        for (const prop in valDict) {
+                            $("#primary-" + val + "-" + prop + "-dropdown").show();
                         }
-                    })
+                    }
                 }
-            }
+            })
         }
 
+        $(".secondary-property-checkbox").hide();
         for (const val in data.secondary_stones) {
             const valDict = data.secondary_stones[val];
             if (Object.keys(valDict).length !== 0) {
+                const idVal = val.replaceAll(" ", "-")
                 for (const prop in valDict) {
-                    const idVal = val.replaceAll(" ", "-")
-                    $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
                     $("#secondary-" + idVal + "-checkbox").change(function () {
-                        $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
                         if ($(this).is(":checked")) {
-                            $("#secondary-" + idVal + "-" + prop + "-dropdown select").show();
+                            $("#secondary-" + idVal + "-" + prop + "-dropdown").show();
                         } else {
-                            $("#secondary-" + idVal + "-" + prop + "-dropdown select").hide();
+                            $("#secondary-" + idVal + "-" + prop + "-dropdown").hide();
                         }
                     })
                 }
